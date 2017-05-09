7d24db20b590c3f62ab7e91939158ec2d260cadf
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-09 18:32:07 +0200

    fix button component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,3 +1,4 @@
+export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as Text } from './Text';
 export { default as Select } from './Select';
