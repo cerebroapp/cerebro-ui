1001c5376e22159df77970059a0b99bc94fb1057
oguhpereira <oguhpereira@protonmail.com>
2023-02-20 14:42:03 -0300

    chore: update build and packages

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,11 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
+const env = process.env.NODE_ENV;
 
 export default [
   {
@@ -22,11 +25,19 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal({
+        includeDependencies: true,
+      }),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      env === 'production' && terser()
     ],
+    globals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+    },
   },
   {
     input: "dist/esm/types/index.d.ts",
