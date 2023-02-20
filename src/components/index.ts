e40e5c62268894f483b8c77052af7a25c1b1e9eb
oguhpereira <oguhpereira@protonmail.com>
2023-02-20 11:06:05 -0300

    refactor: migrate to tsx and add index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,4 @@ export { default as KeyboardNavItem } from './KeyboardNavItem';
 export { default as focusableSelector } from './focusableSelector';
 export { default as FileIcon } from './FileIcon';
 export { default as SmartIcon } from './SmartIcon';
-import * as FormComponents from './Form';
-export { FormComponents };
+export * as FormComponents from './Form';
