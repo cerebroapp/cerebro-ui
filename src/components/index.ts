7992ff6968b925a7ff2a3a97fa8562d433373792
oguhpereira <oguhpereira@protonmail.com>
2023-02-20 11:07:18 -0300

    fix: utils import and export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-export { default as Loading } from './Loading';
-export { default as KeyboardNav } from './KeyboardNav';
-export { default as KeyboardNavItem } from './KeyboardNavItem';
-export { default as focusableSelector } from './focusableSelector';
-export { default as FileIcon } from './FileIcon';
-export { default as SmartIcon } from './SmartIcon';
-export * as FormComponents from './Form';
+export { default as Loading } from './Loading'
+export { default as KeyboardNav } from './KeyboardNav'
+export { default as KeyboardNavItem } from './KeyboardNavItem'
+export { default as FileIcon } from './FileIcon'
+export { default as SmartIcon } from './SmartIcon'
+export * as FormComponents from './Form'
+export * from './utils'
